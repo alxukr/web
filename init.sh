a46eb549e76b03a9ef5f6efbb89f1d248dc030f1
alxukr <alxnet@ukr.net>
2020-12-18 21:48:17 +0000

    add django

--- init.sh
+++ init.sh
@@ -4,6 +4,10 @@ sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test
 
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/djask.py /etc/gunicorn.d/djask.py
+
 sudo /etc/init.d/nginx restart
 #sudo nginx
-sudo gunicorn -c /etc/gunicorn.d/hello.py hello:app
+sudo gunicorn -D -c /etc/gunicorn.d/hello.py hello:app
+sudo gunicorn -D -c /etc/gunicorn.d/djask.py ask.wsgi:application
+#sudo /etc/init.d/gunicorn strat
