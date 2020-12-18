43de3b508b50135b7487d4ae85055fc00ae18878
alxukr <alxnet@ukr.net>
2020-12-18 22:40:27 +0000

    v2

--- init.sh
+++ init.sh
@@ -8,6 +8,7 @@ sudo ln -sf /home/box/web/etc/djask.py /etc/gunicorn.d/djask.py
 
 sudo /etc/init.d/nginx restart
 #sudo nginx
+sudo /etc/init.d/gunicorn stop
 sudo gunicorn -D -c /etc/gunicorn.d/hello.py hello:app
 sudo gunicorn -D -c /etc/gunicorn.d/djask.py ask.wsgi:application
 #sudo /etc/init.d/gunicorn strat
