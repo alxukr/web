2751d2b4b687a6e3e5b91de5f753b894ca10a20d
alxukr <alxnet@ukr.net>
2020-12-26 02:04:50 +0000

    fix

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 class AnswerForm(forms.Form):
     text = forms.CharField(widget = forms.Textarea)
-    question = forms.CharField(max_length = 100)
+    question = forms.IntegerField()
 
 class AskForm(forms.Form):
     title = forms.CharField(max_length = 50)
